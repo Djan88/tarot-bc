f4decd883788d6675cb8b1285111cfeb5dba5412
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-24 19:13:48 +0300

    front-16

--- wp-content/themes/tarot-bc/header.php
+++ wp-content/themes/tarot-bc/header.php
@@ -75,7 +75,7 @@
                   <!-- Collect the nav links, forms, and other content for toggling -->
                   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <div class="close_menu">×</div>
-                    <a class="menu_logo" href="/"><img src="<?php bloginfo('template_url'); ?>/img/school_logo_new.png" alt="" class="img-responsive logo"></a>
+                    <a class="menu_logo" href="/"><img src="<?php bloginfo('template_url'); ?>/img/fav180.png" alt="" class="img-responsive logo"></a>
                   <!-- <div id="bs-example-navbar-collapse-1"> -->
                     
                     <ul class="nav navbar-nav navbar-left">
