8c509d7119f068f8b1d420286b78b6a7173d1b38
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-24 20:30:11 +0300

    front-31

--- wp-content/themes/tarot-bc/header.php
+++ wp-content/themes/tarot-bc/header.php
@@ -54,14 +54,8 @@
           <a href="/"><img src="<?php bloginfo('template_url'); ?>/img/fav180.png" alt="" class="img-responsive logo"></a>
         </div>
         <div class="col-xs-12 col-sm-3 col-md-2 header_contacts pull-right">
-          <?php if (is_user_logged_in()) { ?>
-            <?php $cur_user = wp_get_current_user(); ?>
-            <a class="menu menu_tel">Привет, <?php echo $cur_user->user_firstname; ?></a>
-            <a class="menu menu_email" href="/logout/?_wpnonce=1fd8ce03de">Выйти</a>
-          <?php } else { ?>
-            <a class="menu menu_tel" href="tel:+74951352548">+7 (495) 135-25-48</a>
-            <a class="menu menu_email" href="mailto:info@chikurov.com">info@chikurov.com</a>
-          <?php } ?>
+        <a class="menu menu_tel" href="tel:+74951352548">+7 (495) 135-25-48</a>
+        <a class="menu menu_email" href="mailto:info@chikurov.com">info@chikurov.com</a>
         </div>
         <div class="col-md-9 col-sm-8 col-xs-12 main-menu">
           <div class="row">
@@ -87,6 +81,11 @@
                     <ul class="nav navbar-nav navbar-left">
                       <li><a class="menu" href="/book">«Лечебное Tarot»</a></li>
                       <li><a class="menu" href="http://school-bc.ru/" target="_blank">Бесплатный Онлайн Курс</a></li>
+                      <?php if (is_user_logged_in()) { ?>
+                        <?php $cur_user = wp_get_current_user(); ?>
+                        <li><a class="menu menu_tel">Привет, <?php echo $cur_user->user_firstname; ?></a></li>
+                        <li><a class="menu menu_email" href="/logout/?_wpnonce=1fd8ce03de">Выйти</a></li>
+                      <?php } ?>
                     </ul>
                   </div><!-- /navbar-collapse -->
                 </div><!-- / .container-fluid -->
