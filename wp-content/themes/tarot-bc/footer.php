e65b26f55ed037f8d801c08634583b1cea9587a1
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-24 19:16:01 +0300

    front-17

--- wp-content/themes/tarot-bc/footer.php
+++ wp-content/themes/tarot-bc/footer.php
@@ -40,7 +40,7 @@
   <?php } ?>
   <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="videoCoursLabel" aria-hidden="true">
       <div class="modal-dialog">
-          <div class="modal-content">
+          <div class="modal-content modal-sm">
               <div class="modal-header">
                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                   <h4 class="modal-title" id="videoCoursLabel">Вход / Регистрация</h4>
