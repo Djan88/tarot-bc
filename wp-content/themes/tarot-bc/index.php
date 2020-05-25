fae3694943f84b9486afb06757c3a7fdc7ed533f
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-25 15:22:24 +0300

    front-52

--- wp-content/themes/tarot-bc/index.php
+++ wp-content/themes/tarot-bc/index.php
@@ -9,17 +9,12 @@
           </div> -->
           <div class="col-md-8">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="container">
-              <div class="row">
-                <div class="col-md-12">
-                  <h2><?php the_title(); ?></h2>
-                </div>
-                <div class="col-md-12 seminar_content">
-                  <?php
-                  the_content(__('(more...)'));
-                  edit_post_link(__('Edit This'));
-                  ?>
-                </div>
+            <div class="row">
+              <div class="col-md-12 seminar_content">
+                <?php
+                the_content(__('(more...)'));
+                edit_post_link(__('Edit This'));
+                ?>
               </div>
             </div>
             <?php endwhile; else: ?>
