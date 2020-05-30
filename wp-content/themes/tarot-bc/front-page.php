4d2abda850f3c768814b444e04d47aed3349ee3a
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-30 12:16:03 +0300

    front-4

--- wp-content/themes/tarot-bc/front-page.php
+++ wp-content/themes/tarot-bc/front-page.php
@@ -15,7 +15,9 @@
               <li>Убрать избыточное напряжение в делах</li>
               <li>Сделать отношения гармоничными и уважительными</li>
             </ol>
-            <p class="pink_text">Зарегистрируйтесь и получите в подарок бесплатный онлайн-курс и возможность читать книгу Лечебное Tarot онлайн</p>
+            <?php if (!is_user_logged_in()) { ?>
+              <p class="pink_text">Зарегистрируйтесь и получите в подарок бесплатный онлайн-курс и возможность читать книгу Лечебное Tarot онлайн</p>
+            <?php } ?>
           </div>
           <a href="/book/1" class="seminars_link btn btn-success btn-lg cur_page_ridden">Читать книгу онлайн</a>
         </div>
@@ -67,29 +69,4 @@
       </div>
     </div>
   </section>
-
-  <section class="seminars seminars_light books" id="books">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h2>Книги Ю.Чикурова</h2>
-        </div>
-        <div class="col-md-7 col-sm-7">
-          <h3>Книги Доктора Чикурова можно <span class="blue_text">скачать</span> бесплатно с сайта "Школы Доктора Чикурова" или <span class="pink_text">приобрести</span> в бумажном издании с подписью автора</h3>
-          <!-- <a target="_blank" href="http://www.yuchikurov.info/biologicheskoe-centrirovanie/" class="btn btn-primary btn-lg">Читать книгу</a> -->
-          <div class="book_btns">
-            <a href="https://chikurov.com/kniga" target="_blank" class="btn btn-primary btn-lg kniga_el">Электронные книги</a>
-            <a target="_blank" href="https://shop-bc.com/collection/books" class="btn btn-pink btn-lg">Бумажные издания</a>
-          </div>
-          <!-- <button class="btn btn-primary btn-lg down_book" data-toggle="modal" data-target="#book_down">Скачать книгу бесплатно</button> -->
-        </div>
-        <div class="col-md-5 col-sm-5 text-center books_title_wrap">
-          <img class="books_item books_item_bc" src="<?php bloginfo('template_url'); ?>/img/oblojka_min.jpg" alt="">
-          <img class="books_item books_item_ps" src="<?php bloginfo('template_url'); ?>/img/psichosomatics_rus.png" alt="">
-          <img class="books_item books_item_tr" src="<?php bloginfo('template_url'); ?>/img/tarot.png" alt="">
-        </div>
-      </div>
-    </div>
-  </section>
-  
 <?php get_footer(); ?>
