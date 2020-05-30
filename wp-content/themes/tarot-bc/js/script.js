d6ba9f33eb47f58d660bcd69080b73c1fa6c6008
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-30 12:29:44 +0300

    front-8

--- wp-content/themes/tarot-bc/js/script.js
+++ wp-content/themes/tarot-bc/js/script.js
@@ -39,38 +39,8 @@ jQuery(document).ready(function () {
     localStorage.setItem('page_ridden', jQuery(this).attr('href'));
     console.log(jQuery(this).attr('href'));
   });
-  // var online_block_h = parseFloat(jQuery('.online_school_text').css('height'));
-  // jQuery('.online .col-md-4').css('height', jQuery('.online').css('height'));
-  // jQuery('.online_school_logo').css('height', online_block_h+'px');
-  // jQuery('.online_school').css('height', online_block_h/4+'px');
-  // jQuery('.online_school').css('marginTop', online_block_h/4+'px');
-  // Фильтрация по городам
-  // jQuery('.btn_siminar_filter').on('click', function(event) {
-  //   jQuery('.btn_siminar_filter').removeClass('active');
-  //   jQuery(this).addClass('active');
-  //   var filter_param_city = jQuery(this).data('filter');
-
-  //   if (filter_param_city == 'all') {
-  //     jQuery('.seminar_in').removeClass('hidden');
-  //   } else if (filter_param_city == 'msk') {
-  //     jQuery('.seminar_in').addClass('hidden');
-  //     jQuery('.seminar_item__msk').removeClass('hidden');
-  //   } else if (filter_param_city == 'spb') {
-  //     jQuery('.seminar_in').addClass('hidden');
-  //     jQuery('.seminar_item__spb').removeClass('hidden');
-  //   } else if (filter_param_city == 'kiev') {
-  //     jQuery('.seminar_in').addClass('hidden');
-  //     jQuery('.seminar_item__kiev').removeClass('hidden');
-  //   } else if (filter_param_city == 'riga') {
-  //     jQuery('.seminar_in').addClass('hidden');
-  //     jQuery('.seminar_item__riga').removeClass('hidden');
-  //   }
-  // });
-
-
   //Получение данных из локального хранилища
-  if(supportsStorage && localStorage.getItem('popupStatus')){
-    popupStatus1 = localStorage.getItem('popupStatus1');
+  if(supportsStorage && localStorage.getItem('page_ridden')){
     page_ridden = localStorage.getItem('page_ridden');
   }
   if (page_ridden) {
