77988df7b115f4d574d2ca025cab7feed2a3937e
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-07-02 19:20:53 +0300

    book-2

--- wp-content/themes/tarot-bc/front-page.php
+++ wp-content/themes/tarot-bc/front-page.php
@@ -20,7 +20,7 @@
             <?php } ?>
           </div>
           <a href="/book/1" class="seminars_link btn btn-success btn-lg cur_page_ridden">Читать книгу онлайн</a>
-          <a href="https://shop-bc.com/product/kniga-lechebnoe-tarot-chikurov-yuv" target="_blank" class="seminars_link btn btn-primary btn-lg">Купить бумажное издание</a>
+          <a href="https://shop-bc.com/product/kniga-lechebnoe-tarot-chikurov-yuv" target="_blank" class="book_link btn btn-primary btn-lg">Купить бумажное издание</a>
         </div>
         <div class="col-md-4 col-sm-4 col-xs-12 text-center">
           <img class="chikurov_img" src="<?php bloginfo('template_url'); ?>/img/tarot.png" alt="Лечебное Tarot">
